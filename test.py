8dc34391a6cf45a2ff80dac40a6d4f1ee421b369
Piragix <90191529+Piragix@users.noreply.github.com>
2021-09-10 13:40:36 +0200

    Added garfield quotes

--- test.py
+++ test.py
@@ -1,2 +1,14 @@
 print("Hejsan svenjsan mister baboonga")
 print("Jonathan, where are my balls Jonathan?")
+
+
+
+
+
+
+
+
+
+location = input("Jonathan, where are my balls Jonathan?" )
+
+print(f"Go to {location}, and bring me my balls Jonathan")
